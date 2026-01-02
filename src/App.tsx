ffe2cbcf3441a3695ccf571a528abc33657d2072
Jlamp23 <jamiemlampert23@gmail.com>
2026-01-02 13:08:37 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -174,7 +174,7 @@ function AppContent() {
         </div>
 
         {/* Navigation */}
-        <nav className="p-4 space-y-4 overflow-y-auto" style={{ maxHeight: 'calc(100vh - 180px)' }}>
+        <nav className="p-4 space-y-4 overflow-y-auto" style={{ height: 'calc(100vh - 220px)' }}>
           {/* Printavo Section */}
           <div>
             {sidebarOpen && (
