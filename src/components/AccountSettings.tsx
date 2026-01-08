fbf22b6fbe179123cfd5ef922622773cee4e012b
Jlamp23 <jamiemlampert23@gmail.com>
2026-01-08 11:05:33 -0500

    Updated AccountSettings.tsx

--- src/components/AccountSettings.tsx
+++ src/components/AccountSettings.tsx
@@ -174,6 +174,7 @@ export function AccountSettings() {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${session.access_token}`,
+          'apikey': import.meta.env.VITE_SUPABASE_ANON_KEY,
         },
       });
 
