3903a9317ad47afb05a714b4ed9b248339f04689
Jlamp23 <jamiemlampert23@gmail.com>
2026-01-29 11:46:50 -0500

    Updated index.ts

--- supabase/functions/ssactivewear-api/index.ts
+++ supabase/functions/ssactivewear-api/index.ts
@@ -27,7 +27,7 @@ async function makePromoStandardsRequest(
   apiKey: string
 ) {
   const soapEnvelope = `<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:shar="http://www.promostandards.org/WSDL/SharedObjects/v2.0.0/">
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
   <soap:Header/>
   <soap:Body>
     ${soapBody}
