55be67e188f9d1ffd64c5a3cbf425f882aa32fa7
Jlamp23 <jamiemlampert23@gmail.com>
2026-02-06 12:01:31 -0500

    Added clone-database-schema.ps1

--- deploy-sync-function.js
+++
@@ -1,4 +0,0 @@
-import { readFileSync } from 'fs';
-
-const content = readFileSync('/tmp/cc-agent/61848443/project/supabase/functions/printavo-sync/index.ts', 'utf-8');
-console.log(JSON.stringify({ content }, null, 2));
