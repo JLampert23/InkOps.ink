859f9e33185dbf07b545790cb6f633be991039eb
Jlamp23 <jamiemlampert23@gmail.com>
2026-02-10 12:38:37 -0500

    Updated index.ts

--- supabase/functions/ssactivewear-api/index.ts
+++ supabase/functions/ssactivewear-api/index.ts
@@ -242,7 +242,7 @@ Deno.serve(async (req: Request) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": authHeader,
+        "Authorization": `Bearer ${supabaseServiceRoleKey}`,
       },
       body: JSON.stringify({
         action: "decrypt",
