e6ba0a879ad84971358ac65b8800e1d7072e939c
Jlamp23 <jamiemlampert23@gmail.com>
2026-03-06 15:45:35 -0500

    Updated live-wholesale-pricing.ts

--- supabase/functions/_shared/live-wholesale-pricing.ts
+++ supabase/functions/_shared/live-wholesale-pricing.ts
@@ -75,10 +75,11 @@ export async function getLiveWholesalePricing(
   <shar:id>${escapedId}</shar:id>
   <shar:password>${escapedPassword}</shar:password>
   <shar:productId>${escapedProductId}</shar:productId>
-  <shar:localizationCountry>US</shar:localizationCountry>
-  <shar:localizationLanguage>en</shar:localizationLanguage>
+  <shar:currency>USD</shar:currency>
   <shar:fobId>${escapedFobId}</shar:fobId>
   <shar:priceType>Net</shar:priceType>
+  <shar:localizationCountry>US</shar:localizationCountry>
+  <shar:localizationLanguage>en</shar:localizationLanguage>
   <shar:configurationType>Blank</shar:configurationType>
 </ns2:GetConfigurationAndPricingRequest>`;
 
