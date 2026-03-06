86dcd57821e38c439da413bf2c4993be99c076e4
Jlamp23 <jamiemlampert23@gmail.com>
2026-03-06 18:17:04 -0500

    Updated IntegrationsDiagnostic.tsx

--- src/components/diagnostics/IntegrationsDiagnostic.tsx
+++ src/components/diagnostics/IntegrationsDiagnostic.tsx
@@ -339,7 +339,8 @@ export function IntegrationsDiagnostic() {
       const response = await fetch(url, {
         method: 'GET',
         headers: {
-          'Authorization': `Bearer ${session.access_token}`,
+          'Authorization': `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
+          'X-User-Token': session.access_token,
           'Content-Type': 'application/json',
         },
       });
