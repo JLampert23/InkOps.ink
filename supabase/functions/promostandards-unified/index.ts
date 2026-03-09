cce5eb74290373f57d39d868e143ad6081b3c342
Jlamp23 <jamiemlampert23@gmail.com>
2026-03-09 18:21:54 -0400

    Updated index.ts

--- supabase/functions/promostandards-unified/index.ts
+++ supabase/functions/promostandards-unified/index.ts
@@ -11,7 +11,7 @@ const PROMOSTANDARDS_ENDPOINTS = {
   productData: "https://promostandards.ssactivewear.com/productdata/v2/productdataservicev2.svc",
   inventory: "https://promostandards.ssactivewear.com/inventory/v2/inventoryservice.svc",
   media: "https://promostandards.ssactivewear.com/mediacontent/v1/mediacontentservice.svc",
-  pricingAndConfiguration: "https://promostandards.ssactivewear.com/pricingandconfiguration/1.0.0/pricingandconfiguration.svc",
+  pricingAndConfiguration: "https://promostandards.ssactivewear.com/pricingandconfiguration/v1/pricingandconfigurationservice.svc",
 };
 
 async function makePromoStandardsRequest(
