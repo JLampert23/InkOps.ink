08b754ba1ff812271f5715ab534e1bba3f433e82
Jlamp23 <jamiemlampert23@gmail.com>
2026-03-10 13:37:40 -0400

    Update index.ts
    update endpoint

--- supabase/functions/promostandards-unified/index.ts
+++ supabase/functions/promostandards-unified/index.ts
@@ -11,7 +11,7 @@ const PROMOSTANDARDS_ENDPOINTS = {
   productData: "https://promostandards.ssactivewear.com/productdata/v2/productdataservicev2.svc",
   inventory: "https://promostandards.ssactivewear.com/inventory/v2/inventoryservice.svc",
   media: "https://promostandards.ssactivewear.com/mediacontent/v1/mediacontentservice.svc",
-  pricingAndConfiguration: https://promostandards.ssactivewear.com/PricingAndConfiguration/1.0.0/PricingAndConfiguration.svc,
+  pricingAndConfiguration: "https://promostandards.ssactivewear.com/PricingAndConfiguration/1.0.0/PricingAndConfiguration.svc",
 };
 
 async function makePromoStandardsRequest(
